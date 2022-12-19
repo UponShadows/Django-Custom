5b48255b1bd246a18611f12006192b2d4f667c5e
UponShadows <uponshadows2@gmail.com>
2022-12-19 12:09:48 +0200

    Added info oso it dowesnt bug with migrations

--- base/core/models.py
+++ base/core/models.py
@@ -3,5 +3,12 @@ from .manager import UserManager
 
 # Create your models here.
 class CustomUser(models.Model):
+    username = None # Removes username field so u login with email 
+    
+    
+    
+    
     objects = UserManager() 
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
     pass
