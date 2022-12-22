c9318854856aa5a5530b02e6b4ed361cd35ba473
UponShadows <uponshadows2@gmail.com>
2022-12-22 15:20:44 +0200

    updated model.

--- base/core/models.py
+++ base/core/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from .manager import UserManager
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class CustomUser(models.Model):
+class CustomUser(AbstractUser):
     username = None # Removes username field so u login with email 
     
     
